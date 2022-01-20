fdb2ee79227aa97f3833c00bcacbee356d6a7ede
ric1709 <abbaskanov@gmail.com>
2022-01-20 19:07:14 +0600

    HW5

---
+++ new-app/src/App.js
@@ -0,0 +1,13 @@
+import Note from './components/jsx/Note';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Note />
+    </div>
+  );
+}
+
+export default App;
